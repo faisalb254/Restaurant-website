815a3bbc14db930c518f8268ea467a5341f3565a
Faisal Bilal <faisalbilal@Faisals-MacBook-Pro.local>
2024-06-27 23:03:32 +0500

    added reponsiveness

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function nav1(){
+    var toggle = document.getElementById("navlist");
+    if (toggle.style.display === 'none') {
+    toggle.style.display = 'block';
+} else {
+    toggle.style.display = 'none';
+}
+}

faf88f8a431b105029b41ca0eed1c1558eda9ff3
Faisal Bilal <faisalbilal@Faisals-MacBook-Pro.local>
2024-08-06 22:59:28 +0500

    added js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,42 @@
 function nav1(){
     var toggle = document.getElementById("navlist");
-    if (toggle.style.display === 'none') {
-    toggle.style.display = 'block';
-} else {
+    if (toggle.style.display === 'block') {
     toggle.style.display = 'none';
+} else {
+    toggle.style.display = 'block';
 }
 }
+function check(){
+    if(window.innerWidth<1000){
+        document.getElementById("navbar").style.backgroundColor='black'
+        document.getElementById("navbar").style.boxShadow='none'
+        document.getElementById("rest").style.color='white'
+        document.getElementById("rest").style.fontSize='24px'
+    }else{
+        navchange();
+    }
+}
+function navchange(){
+    if(window.scrollY>100){
+        document.getElementById("navbar").style.backgroundColor='white'
+        document.getElementById("navbar").style.boxShadow='grey 0px 0px 5px'
+        document.getElementById("rest").style.color='black'
+        document.getElementById("rest").style.fontSize='30px'
+        let navlinks= document.querySelectorAll(".navtags")
+        navlinks.forEach(x => {
+            x.style.color='black';
+        });
+    }else
+    if(window.scrollY<100){
+        document.getElementById("navbar").style.backgroundColor='transparent'
+        document.getElementById("navbar").style.boxShadow='none'
+        document.getElementById("rest").style.color='white'
+        document.getElementById("rest").style.fontSize='24px'
+        let navlinks= document.querySelectorAll(".navtags")
+        navlinks.forEach(x => {
+            x.style.color='white';
+        });
+    }
+}
+
+document.addEventListener("wheel", check);
